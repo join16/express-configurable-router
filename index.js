69d6689438dca1d34ca574aefd1c67b442f52585
Join <choin1116@gmail.com>
2016-07-17 18:37:16 +0900

    Few Changes
    - Fix validate plugin parser method
    - Fix utils.url.join to remove last slash
    - Add docs key in not allowed keys
    - Implement authority plugin

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 var ExpressiveRoute = require('./lib/expressive-route');
 var Plugin = require('./lib/plugin');
 var validate = require('./lib/plugins/validate');
+var authority = require('./lib/plugins/authority');
 
 module.exports = ExpressiveRoute;
 module.exports.Plugin = Plugin;
 
 module.exports.plugins = {
-  validate: validate
+  validate: validate,
+  authority: authority
 };
